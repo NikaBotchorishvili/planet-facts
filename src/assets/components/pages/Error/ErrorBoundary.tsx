3e8f73c44a6a517f7d95334ed6de0e03243ec8f0
NikaBotchorishvili <nikabotchorishvili@gmail.com>
2023-05-25 16:12:28 +0400

    error boundary error fixes

--- src/assets/components/pages/Error/ErrorBoundary.tsx
+++ src/assets/components/pages/Error/ErrorBoundary.tsx
@@ -1,17 +1,27 @@
 import { useRouteError } from "react-router-dom";
 
-
-
 function ErrorBoundary() {
-    const error = useRouteError();
-    return (
-        <div className="error">
-            <h1>Whoops, {error.statusText}</h1>
-            <p>{error.data}</p>
-            <small className="back-element"><a href="/">Click Here</a> to go back to main page</small>
-            <small>status: {error.status}</small>
-        </div>
-    );
+	const error = useRouteError();
+	return (
+		<section className="error">
+			<h1>
+				Whoops,{" "}
+				{(error as Error)?.message ||
+					(error as { statusText?: string })?.statusText}
+			</h1>
+			<p>
+				{(error as Error)?.message ||
+					(error as { data?: string })?.data}
+			</p>
+			<small className="back-element">
+				<a href="/">Click Here</a> to go back to main page
+			</small>
+			<small>
+				{(error as Error)?.message ||
+					(error as { status?: string })?.status}
+			</small>
+		</section>
+	);
 }
 
 export default ErrorBoundary;
